8a880702cbd028e72c0355b087a47559b51a81cf
Justin Heyns <justin.heyns@digiata.local>
2017-10-30 14:09:01 +0200

    removed example markdown

--- docs/frontMatterInjector.sh
+++ docs/frontMatterInjector.sh
@@ -1,14 +1,40 @@
 #!/bin/bash
 find | grep "Index.md" | while read -r line
 do
+    fileContents=$(< $line)
     noIndex=${line%%/Index.md}
     component=${noIndex##*/}
     noComponent=${noIndex%/*}
     feature=${noComponent##*/}
     noFeature=${noComponent%/*}
     group=${noFeature##*/}
-    echo "Group: $group, Feature: $feature, Component: $component"  
-    #TODO: get group as well
+    stringWithNewLines="Group: $group"$'\n'"Feature: $feature\n Component: $component"  
+
+    block="---"$'\n'
+    block="${block}layout: docs"$'\n'
+    block="${block}title: ${component}"$'\n'
+    block="${block}description: ${component}"$'\n'
+    block="${block}group: ${group}"$'\n'
+    block="${block}feature: ${feature}"$'\n'
+    block="${block}component: ${component}"$'\n'
+    block="${block}toc: true"$'\n'
+    block="${block}redirect_from: docs/${group}/${feature}/${component}/index"$'\n'
+    block="${block}---"$'\n'
+    fileContents="${block}${fileContents}"
+    echo "$fileContents">"$line.example"
+    #TODO: read contents of each line into a variable and then prepend block to the content and then write it back to the file
 done
 
 
+: <<'COMMENT'
+---
+layout: docs
+title: Licence
+description: Look at this function
+group: linx5main
+feature: Functions
+component: ForEach
+toc: true
+redirect_from: "/docs/linx5main/functions/foreach/index"
+---
+COMMENT
