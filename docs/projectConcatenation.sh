aca4c825519fe258664dab712d77086a03c8526e
Justin Heyns <justin.heyns@digiata.local>
2017-11-01 11:17:49 +0200

    dropped redundant naming info in documentation folder

---
+++ docs/projectConcatenation.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+ls -d */ | while read -r line
+do
+    noLinx=${line##linx5-components-}
+    echo $noLinx
+    mv $line $noLinx
+    # fileContents=$(< $line)
+    # noIndex=${line%%/Index.md}
+    # component=${noIndex##*/}
+    # noComponent=${noIndex%/*}
+    # feature=${noComponent##*/}
+    # noFeature=${noComponent%/*}
+    # group=${noFeature##*/}
+    # stringWithNewLines="Group: $group"$'\n'"Feature: $feature\n Component: $component"  
+
+    # block="---"$'\n'
+    # block="${block}layout: docs"$'\n'
+    # block="${block}title: ${component}"$'\n'
+    # block="${block}description: ${component}"$'\n'
+    # block="${block}group: ${group}"$'\n'
+    # block="${block}feature: ${feature}"$'\n'
+    # block="${block}component: ${component}"$'\n'
+    # block="${block}toc: true"$'\n'
+    # block="${block}redirect_from: docs/${group}/${feature}/${component}/index"$'\n'
+    # block="${block}---"$'\n'
+    # fileContents="${block}${fileContents}"
+    # echo "$fileContents">"$line"
+done
